25239f269acfc45850165ee42755101e0522cbcd
Dan_Shields <d.shieldsuk@gmail.com>
2017-03-29 02:39:31 +0100

    Moving serial writes to a new function

--- main_pi.py
+++ main_pi.py
@@ -4,13 +4,12 @@ from picamera import PiCamera
 import time
 import cv2
 import numpy as np
-from collections import deque
 import serial
 
-#TODO: change address to a linux specific one
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
-lastDirection = "4"
+lastDirection = True # 1= left, 0= right
+left = right = headingForward = pivot = False
 
 camera = PiCamera()
 camera.resolution = (640, 480)
@@ -40,36 +39,26 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     imgray = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
     ret, thresh = cv2.threshold(imgray, 127, 255, 0)
     contours, _ = cv2.findContours(imgray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    ball_radius = 0
 
-    if (contours):
-    #for cnt in contours:
+    if contours:
+        # for cnt in contours:
         cnt = max(contours, key=cv2.contourArea)
         ((x, y), radius) = cv2.minEnclosingCircle(cnt)
 
         center = None
-        
-        if (radius > 10):
-            cv2.circle(img, (int(x), int(y)), int(radius), [0, 0, 255], 2)
-            center = (int(x),int(y))
 
-            if (x < 220):
-                print "turning left"
-                lastDirection = "3"
-                ser.write('3')
-            elif (x > 420):
-                print "turning right"
-                lastDirection = "4"
-                ser.write('4')
-            else:
-                print "going straight"
-                ser.write('1')
-	else:
-	    print "ball not found, turning " + lastDirection
-	    ser.write(lastDirection)
+        if radius > 10:
+            #ball has been found
+            left = False
+            right = False
+
+
+        else:
+            print "ball not found, turning " + lastDirection
+            ser.write(lastDirection)
     else:
-       	print "ball not found, turning " + lastDirection
-       	ser.write(lastDirection)
+        print "ball not found, turning " + lastDirection
+        ser.write(lastDirection)
 
     rawCapture.truncate(0)
 
@@ -78,3 +67,6 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
         break
 ser.write('0')
 cv2.destroyAllWindows()
+
+
+def SendData(controls, turnSpeed):
