d6ae941609f849ff9763b57cc114b3bdb32733cc
Dan_Shields <d.shieldsuk@gmail.com>
2017-06-27 12:44:13 +0100

    Small changes

--- serial_test/serial_test.ino
+++ serial_test/serial_test.ino
@@ -4,10 +4,10 @@ void setup() {
 
 void loop() {
   if(Serial.available() == 3) {
-    unsigned char data[3];
+    char data[3];
     data[0] = Serial.read();
     data[1] = Serial.read();
     data[2] = Serial.read();
-    Serial.write(data);
+    Serial.write(data, 3);
   }
 }
--- serial_test/serial_test.py
+++ serial_test/serial_test.py
@@ -2,11 +2,10 @@
 import numpy as np
 import serial
 
-
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=0.050, bytesize=8)
 
-a = np.packbits([1,0,1,0], axis=0)
-b = np.append(a,[255,128])
+a = np.packbits([1, 0, 1, 0], axis=0)
+b = np.append(a, [255, 128])
 
 data = bytearray(b)
 
