b9be71707a92c1851f91391afe7b79cefaf424b3
Dan_Shields <d.shieldsuk@gmail.com>
2017-06-27 18:20:33 +0100

    First test

---
+++ controller-test.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import numpy as nps
+import serial
+import xbox
+joy = xbox.Joystick()
+
+ser = serial.Serial('/dev/ttyAMC0', 9600, timeout=0.050, bytesize=8)
+
+enable = 1
+
+while not joy.Back()
+    throttle = joy.rightTrigger()
+    brake = joy.leftTrigger()
+    x_axis = joy.leftX()
+
+    global_speed = int(abs(throttle - brake) * 255)
+    move = int(global_speed > 20)
+    F_B = int(throttle > brake)
+
+    turn_speed = 255 - int(abs(x_axis) * 255)
+    L = int(x_axis < 0)
+    R = int(x_axis > 0)
+
+    a = np.packbits([enable, move, F_B, L, R], axis=0)
+    b = np.append(a, [turn_speed, global_speed])
+
+    data = bytearray(iter(b))
+
+    #write data if previous data was received
+    if ser.inWaiting() > 0:
+        ser.write(data)
+        ser.read()
+
+a = np.packbits([0], axis=0)
+b = np.append(a, [0, 0])
+
+data = bytearray(iter(b))
+
+ser.write(data)
+
+ser.close()
+joy.close()
