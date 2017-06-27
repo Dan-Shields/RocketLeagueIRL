3d471a7121e99eb696346a94a5b6e43f2745bfbc
Dan_Shields <d.shieldsuk@gmail.com>
2017-06-27 23:53:56 +0100

    Began AI stuff

--- main_pc.py
+++ main_pc.py
@@ -25,19 +25,29 @@ def send_handshake():
         else if time.time() - start_time > 1:
             sys.exit("Handshake failed or connection was lost")
 
-ser = serial.Serial('COM3', 9600), timeout=0.050, bytesize=8
+ser = serial.Serial('COM3', 9600, timeout=0.050, bytesize=8)
 
 send_handshake()
 
 enable = 1
 
 cap = cv2.VideoCapture(0)
+
+IMG_WIDTH = 640
+IMG_HEIGHT = 480
+
+cap.set(CV_CAP_PROP_FRAME_WIDTH,IMG_WIDTH);
+cap.set(CV_CAP_PROP_FRAME_HEIGHT,IMG_HEIGHT);
+
 pts = deque(maxlen=64)
 while True:
     if ser.inWaiting():
         if ser.read == 63:
             # reconnect was requested
             send_handshake()
+
+    ball_found = False
+    goal_found = False
 
     # Take each frame
     _, img = cap.read()
@@ -60,8 +70,6 @@ while True:
     ret, thresh = cv2.threshold(blurred, 127, 255, 0)
     _, contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-
-
     if (contours):
     #for cnt in contours:#
         #cnt1 = max(contours, key=cv2.contourArea)
@@ -80,11 +88,13 @@ while True:
             approx2 = cv2.approxPolyDP(cnt[1],epsilon2,True)
             (x1,y1),(w1,h1),_ = cv2.minAreaRect(approx2)
 
-
-
         center = None
 
         if (w > 20):
+            ballx = 0
+            ball_width = 0
+            goalx = 0
+
             #cv2.circle(img, (int(x), int(y)), int(radius), [0, 0, 255], 2)
             #center = (int(x),int(y))
             box1 = cv2.boxPoints(cv2.minAreaRect(approx1))
@@ -96,6 +106,10 @@ while True:
                 box2 = np.int0(box2)
                 cv2.drawContours(img, [box2],0,(255,255,0),2)
                 cv2.drawContours(img,[approx2], 0,(255,0,0), 2)
+
+                ball_found = True
+                goal_found = True
+
                 """if (h1 == 0):
                     continue"""
                 """if (w1/h1 > w/h):
@@ -109,10 +123,20 @@ while True:
                 if ((w/h > 5 or h/w > 5) or len(approx1) < 7):
                     shape = "goal"
                     shape1 = "ball"
+
+                    ballx = x1
+                    ball_width = w1
+                    goalx = x
+
                     print "Goal Height %d" %int(h1) + " Width %d" %int(w1) + " Ratio %d" %int(w1/h1) + "Ball Height %d" %int(h) + " Width %d" % int(w) + " Ratio: %d" %int(w/h)
                 else:
                     shape = "ball"
                     shape1 = "goal"
+
+                    ballx = x
+                    ball_width = w
+                    goalx = x1
+
                     print "Goal Height %d" %int(h) + " Width %d" %int(w) + " Ratio %d" %int(w/h) + "Ball Height %d" %int(h1) + " Width %d" % int(w1) + " Ratio: %d" %int(w1/h1)
 
                 cv2.putText(img, shape, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
@@ -120,11 +144,31 @@ while True:
             else:
                 if ((w/h > 5 or h/w > 5) or len(approx1) < 7):
                     shape = "goal"
+                    goalx = x
+                    goal_found = True
                 else:
                     shape = "ball"
+                    ballx = x
+                    ball_width = w
+                    ball_found = True
                 cv2.putText(img, shape, (int(x),int(y)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 2)
             cv2.drawContours(img,[box1],0,(0,0,255),2)
             cv2.drawContours(img,[approx1],0,(0,255,0),2)
+
+    if ball_found and goal_found:
+        if ballx > goalx and goalx > IMG_WIDTH - 50:
+            control_array = [enable, 1, 1, 0, 1]
+            send_data(control_array, 128, 255)
+        else if ballx > goalx and goalx < IMG_WIDTH - 50
+            control_array = [enable, 1, 1, 0, 0]
+            send_data(control_array, 0, 128)
+    else:
+        # always send a command to not move to keep-alive connection
+        control_array = [enable, 0, 1, 0, 1]
+        send_data(control_array, 0, 0)
+
+
+
 
     #cv2.imshow('hsv', hsv)
     #cv2.imshow('mask', mask)
@@ -136,4 +180,3 @@ while True:
         break
 #ser.write('0')
 cv2.destroyAllWindows()
->>>>>>> opencv-testing
