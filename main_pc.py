7696a5e837fc7873cfccc4d4cc2c4f535e914c4c
chrisfeghali <chrisfeghali@live.com>
2017-06-27 18:00:21 +0100

    Goal Detection Progress

--- main_pc.py
+++ main_pc.py
@@ -3,6 +3,9 @@ import cv2
 import numpy as np
 from collections import deque
 
+def getcontours(contours, n):
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)
+    return contours[:n]
 #ser = serial.Serial('COM3', 1200)
 
 lastDirection = "4"
@@ -28,34 +31,67 @@ while True:
     res = cv2.bitwise_and(img, img, mask=mask)
 
     imgray = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
-    ret, thresh = cv2.threshold(imgray, 127, 255, 0)
-    _, contours, _ = cv2.findContours(imgray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    blurred = cv2.GaussianBlur(imgray, (5,5), 0)   
+    ret, thresh = cv2.threshold(blurred, 127, 255, 0)
+    _, contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    print(contours)
 
 
-    x = 2
-    #if (contours):
-    for cnt in contours:#
-        x = x - 1
-        epsilon = 0.1*cv2.arclength(cnt, True)
-        approx = cv2.approxPolyDP(cnt,epsilon,TRUE)
+    if (contours):
+    #for cnt in contours:#
+        #cnt1 = max(contours, key=cv2.contourArea)
+        #contours.remove(max(contours, key=cv2.contourArea))
+        #cnt2 = max(contours, key=cv2.contourArea)
+        cnt = getcontours(contours, 2)
+        print(cnt)
+        epsilon1 = 0.01*cv2.arcLength(cnt[0], True)
+        approx1 = cv2.approxPolyDP(cnt[0],epsilon1,True)
         #((x, y), radius) = cv2.minEnclosingCircle(cnt)
-        x,y,w,h = cv2.boundingRect(approx)
+        x,y,w,h = cv2.boundingRect(approx1)
+        w1 = 0
+        if (len(cnt) > 1):
+            epsilon2 = 0.01*cv2.arcLength(cnt[1], True)
+            approx2 = cv2.approxPolyDP(cnt[1],epsilon2,True)
+            x1,y1,w1,h1 = cv2.boundingRect(approx2)
+
+
 
         center = None
 
-        if (w > 10):
+        if (w > 50):
             #cv2.circle(img, (int(x), int(y)), int(radius), [0, 0, 255], 2)
             #center = (int(x),int(y))
-            cv2.rectangle(img,(int(x),int(y)),(int(x+w),int(y+h)),[0,255,0],2)
-            centerx = int((x+w)/2)
-            centery = int((y+h)/2)
+            box1 = cv2.boxPoints(cv2.minAreaRect(approx1))
+            box1 = np.int0(box1)
+            if (w1> 50): 
+                box2 = cv2.boxPoints(cv2.minAreaRect(approx2))
+                box2 = np.int0(box2)
+                cv2.drawContours(img, [box2],0,(255,255,0),2)
+                cv2.drawContours(img,[approx2], 0,(255,0,0), 2)
+                if (w1/h1 > w/h):
+                    shape1 = "goal"
+                    shape = "ball"
+                else:
+                    shape1 = "ball"
+                    shape = "goal"
+                cv2.putText(img, shape, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+                cv2.putText(img, shape1, (x1,y1), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2) 
+            cv2.drawContours(img,[box1],0,(0,0,255),2)
+            cv2.drawContours(img,[approx1],0,(0,255,0),2)
+            
+
+                
+
+                  
 
 
-            if (centerx < 220):
+
+            """if (x < 220):
                 print "turning left"
                 lastDirection = "6"
                 #ser.write('3')
-            elif (centerx > 420):
+            elif (x > 420):
                 print "turning right"
                 lastDirection = "5"
                 #ser.write('4')
@@ -64,10 +100,7 @@ while True:
                 #ser.write('1')
         else:
             print "ball not found, turning " + lastDirection
-            #ser.write(lastDirection)
-        if (x == 0):
-            x = 2
-            break
+            #ser.write(lastDirection)s"""
 
     #else:
     #    print "ball not found, turning " + lastDirection
