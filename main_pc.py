3a0734068bccdc353cf8ccbf145c67d963e446c8
chrisfeghali <chrisfeghali@live.com>
2017-06-27 22:58:06 +0100

    Good Enough

--- main_pc.py
+++ main_pc.py
@@ -42,42 +42,62 @@ while True:
         #cnt1 = max(contours, key=cv2.contourArea)
         #contours.remove(max(contours, key=cv2.contourArea))
         #cnt2 = max(contours, key=cv2.contourArea)
+        h1 = 1
+        h = 1
         cnt = getcontours(contours, 2)
         epsilon1 = 0.01*cv2.arcLength(cnt[0], True)
         approx1 = cv2.approxPolyDP(cnt[0],epsilon1,True)
         #((x, y), radius) = cv2.minEnclosingCircle(cnt)
-        x,y,w,h = cv2.boundingRect(approx1)
+        (x,y),(w,h),_ = cv2.minAreaRect(approx1)
         w1 = 0
         if (len(cnt) > 1):
             epsilon2 = 0.01*cv2.arcLength(cnt[1], True)
             approx2 = cv2.approxPolyDP(cnt[1],epsilon2,True)
-            x1,y1,w1,h1 = cv2.boundingRect(approx2)
+            (x1,y1),(w1,h1),_ = cv2.minAreaRect(approx2)
 
 
 
         center = None
 
-        if (w > 50):
+        if (w > 20):
             #cv2.circle(img, (int(x), int(y)), int(radius), [0, 0, 255], 2)
             #center = (int(x),int(y))
             box1 = cv2.boxPoints(cv2.minAreaRect(approx1))
             box1 = np.int0(box1)
-            if (w1> 50): 
+            """if (int(h) == 0):
+                continue"""
+            if (w1> 20): 
                 box2 = cv2.boxPoints(cv2.minAreaRect(approx2))
                 box2 = np.int0(box2)
                 cv2.drawContours(img, [box2],0,(255,255,0),2)
                 cv2.drawContours(img,[approx2], 0,(255,0,0), 2)
-                if (w1/h1 > w/h):
+                """if (h1 == 0):
+                    continue"""
+                """if (w1/h1 > w/h):
                     shape1 = "goal"
                     shape = "ball"
                     print "Goal Height %d" %int(h1) + " Width %d" %int(w1) + " Ratio %d" %int(w1/h1) + "Ball Height %d" %int(h) + " Width %d" % int(w) + " Ratio: %d" %int(w/h)        
 
                 else:
                     shape1 = "ball"
+                    shape = "goal" """
+                if ((w/h > 5 or h/w > 5) or len(approx1) < 7):
                     shape = "goal"
+                    shape1 = "ball"
+                    print "Goal Height %d" %int(h1) + " Width %d" %int(w1) + " Ratio %d" %int(w1/h1) + "Ball Height %d" %int(h) + " Width %d" % int(w) + " Ratio: %d" %int(w/h)
+                else:
+                    shape = "ball"
+                    shape1 = "goal"
                     print "Goal Height %d" %int(h) + " Width %d" %int(w) + " Ratio %d" %int(w/h) + "Ball Height %d" %int(h1) + " Width %d" % int(w1) + " Ratio: %d" %int(w1/h1)
-                cv2.putText(img, shape, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-                cv2.putText(img, shape1, (x1,y1), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2) 
+
+                cv2.putText(img, shape, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+                cv2.putText(img, shape1, (int(x1),int(y1)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2) 
+            else:
+                if ((w/h > 5 or h/w > 5) or len(approx1) < 7):
+                    shape = "goal"
+                else:
+                    shape = "ball"
+                cv2.putText(img, shape, (int(x),int(y)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 2)
             cv2.drawContours(img,[box1],0,(0,0,255),2)
             cv2.drawContours(img,[approx1],0,(0,255,0),2)
             
