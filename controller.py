965c3e16f28b442b0ff9b4f9fa7bd06844c70c1b
Dan_Shields <d.shieldsuk@gmail.com>
2017-06-28 15:50:44 +0100

    It "might" work

--- controller.py
+++ controller.py
@@ -2,73 +2,94 @@
 import numpy as np
 import serial
 import socket
+import simplejson as json
+import time
+
+def send_data(control_array, turn_speed_int, global_speed_int):
+    if SERIAL_ENABLED:
+        a = np.packbits(control_array, axis=0)
+        b = np.append(a, [turn_speed_int, global_speed_int])
+        data = bytearray(iter(b))
+
+        ser.write(data)
+
+def send_handshake():
+    if SERIAL_ENABLED:
+        print "Sending handshake"
+        start_time = time.time()
+        ser.write([127])
+
+        while True:
+            if ser.inWaiting() > 0:
+                print "Handshake returned"
+            elif time.time() - start_time > 1:
+                break
+                sys.exit("Handshake failed or connection was lost")
+    else:
+        print "Serial disabled"
+
+def stop_movement():
+    if SERIAL_ENABLED:
+        control_array = [1, 0, 1, 0, 1]
+        send_data(control_array, 0, 0)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = socket.gethostname()
+host = "0.0.0.0"
 port = 26656
 s.bind((host, port))
 
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=0.050, bytesize=8)
+
+send_handshake()
+print "Sending handshake"
+
+enable = 1
+
 print "Listening on address " + host
 
-s.listen(5)
+s.listen(1)
+
+conn, addr = s.accept()
+print 'Connection address: ', addr
+last_activity = time.time()
+
 while True:
-  c, addr = s.accept()
-  print ('Got connection from',addr)
-  c.send('Thank you for connecting')
-  c.close()
+    data = conn.recv(1024)
+    if not data and time.time() - last_activity > 1: break
+    print "received data: ", data
+    last_activity = time.time()
+    conn.send(data)
+    
+    joy = json.loads(data)
 
-# ser = serial.Serial('/dev/ttyACM0', 9600, timeout=0.050, bytesize=8)
+    throttle = joy['rt']
+    brake = joy['lt']
+    x_axis = joy['x']
 
-# a = np.packbits([0, 1, 1, 1, 1, 1, 1, 1], axis=0)
-# b = np.append(a, [255, 255])
+    global_speed = int(abs(throttle - brake) * 255)
+    move = int(global_speed > 20)
+    F_B = int(throttle > brake)
 
-# data = bytearray(iter(b))
-# ser.write(data)
-# print "Sending handshake"
+    turn_speed = 255 - int(abs(x_axis) * 200)
+    L = int(x_axis < 0)
+    R = int(x_axis > 0)
 
-# enable = 1
+    if F_B == 0 and (L == 1 or R == 1):
+        L = int(not bool(L))
+        R = int(not bool(R))
 
-# while not joy.Back():
-#     throttle = joy.rightTrigger()
-#     brake = joy.leftTrigger()
-#     x_axis = joy.leftX()
+    if not joy['b']:
+        global_speed = int(global_speed / 2)
 
-#     global_speed = int(abs(throttle - brake) * 255)
-#     move = int(global_speed > 20)
-#     F_B = int(throttle > brake)
+    send_data([enable, move, F_B, L, R], turn_speed, global_speed)
 
-#     turn_speed = 255 - int(abs(x_axis) * 200)
-#     L = int(x_axis < 0)
-#     R = int(x_axis > 0)
 
-#     if F_B == 0 and (L == 1 or R == 1):
-#         L = int(not bool(L))
-#         R = int(not bool(R))
+    if move == 1 and F_B == 1:
+        print "Moving forwards"
+    elif move == 1 and F_B == 0:
+        print "Moving backwards"
 
-#     if not joy.B():
-#         global_speed = int(global_speed / 2)
+stop_movement()
 
-#     a = np.packbits([enable, move, F_B, L, R], axis=0)
-#     b = np.append(a, [turn_speed, global_speed])
-
-#     data = bytearray(iter(b))
-
-#     if move == 1 and F_B == 1:
-#         print "Moving forwards"
-#     elif move == 1 and F_B == 0:
-#         print "Moving backwards"
-
-#     #write data if previous data was received
-#     if ser.inWaiting() > 0:
-#         print "Wrote data"
-#         ser.write(data)
-#         ser.read()
-
-# a = np.packbits([0], axis=0)
-# b = np.append(a, [0, 0])
-
-# data = bytearray(iter(b))
-
-# ser.write(data)
-
-# ser.close()
+ser.close()
+conn.close()
