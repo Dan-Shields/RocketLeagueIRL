2f06e9b51f56bf99f48ceeffbd8685b1bdcf6705
Dan_Shields <d.shieldsuk@gmail.com>
2017-06-28 12:24:57 +0100

    ai on the pi

--- main_pi.py
+++ main_pi.py
@@ -11,14 +11,53 @@ def getcontours(contours, n):
     contours = sorted(contours, key=cv2.contourArea, reverse=True)
     return contours[:n]
 
+def send_data(control_array, turn_speed_int, global_speed_int):
+    a = np.packbits(control_array, axis=0)
+    b = np.append(a, [turn_speed_int, global_speed_int])
+    data = bytearray(iter(b))
+
+    ser.write(data)
+
+def send_handshake():
+    print "Sending handshake"
+    start_time = time.time()
+    ser.write([127])
+
+    while True:
+        if ser.inWaiting() > 0:
+            print "Handshake returned"
+            break
+        elif time.time() - start_time > 1:
+            sys.exit("Handshake failed or connection was lost")
+
+def stop_movement():
+    control_array = [enable, 0, 1, 0, 1]
+    send_data(control_array, 0, 0)
+
+ser = serial.Serial('COM3', 9600, timeout=0.050, bytesize=8)
+
+send_handshake()
+
+enable = 1
+
+IMG_WIDTH = 320
+IMG_HEIGHT = 240
+
 camera = PiCamera()
-camera.resolution = (320, 240)
+camera.resolution = (IMG_WIDTH, IMG_HEIGHT)
 camera.framerate = 32
 
-rawCapture = PiRGBArray(camera, size=(320, 240))
+rawCapture = PiRGBArray(camera, size=(IMG_WIDTH, IMG_HEIGHT))
 
 time.sleep(0.1)
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    if ser.inWaiting():
+        if ser.read == 63:
+            # reconnect was requested
+            send_handshake()
+
+    ball_found = False
+    goal_found = False
 
     # Take each frame
     img = frame.array
@@ -61,29 +100,69 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
         if (w > 5):
             #box1 = cv2.boxPoints(cv2.minAreaRect(approx1))
             #box1 = np.int0(box1)
+
+            ballx = 0
+            ball_width = 0
+            goalx = 0
+
             if(w1 > 5):
             	#box2 = cv2.boxPoints(cv2.minAreaRect(approx2))
                 #box2 = np.int0(box2)
                 #cv2.drawContours(img, [box2],0,(255,255,0),2)
                 #cv2.drawContours(img,[approx2], 0,(255,0,0), 2)
+
+                ball_found = True
+                goal_found = True
+
                 if ((w/h > 5 or h/w > 5) or len(approx1) < 7):
                     shape = "goal"
                     shape1 = "ball"
+
+                    ballx = x1
+                    ball_width = w1
+                    goalx = x
+
                     print "Goal Height %d" %int(h) + " Width %d" %int(w) + " Ratio %d" %int(w/h) + "Ball Height %d" %int(h) + " Width %d" % int(w) + " Ratio: %d" %int(w/h)
                 else:
                     shape = "ball"
                     shape1 = "goal"
+
+                    ballx = x
+                    ball_width = w
+                    goalx = x1
+
                     print "Goal Height %d" %int(h) + " Width %d" %int(w) + " Ratio %d" %int(w/h) + "Ball Height %d" %int(h1) + " Width %d" % int(w1) + " Ratio: %d" %int(w1/h1)
                 #cv2.putText(img, shape, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
                 #cv2.putText(img, shape1, (int(x1),int(y1)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2) 
             else:
                 if ((w/h > 5 or h/w > 5) or len(approx1) < 7):
                     shape = "goal"
+                    goalx = x
+                    goal_found = True
                 else:
                     shape = "ball"
+                    ballx = x
+                    ball_width = w
+                    ball_found = True
                 #cv2.putText(img, shape, (int(x),int(y)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 2)
                 #cv2.drawContours(img,[box1],0,(0,0,255),2)
                 #cv2.drawContours(img,[approx1],0,(0,255,0),2)
+
+    if ball_found and goal_found:
+        if ballx > goalx and goalx < 50:
+            #turn left until > goal is > 50px from the left
+            control_array = [enable, 1, 1, 1, 0]
+            send_data(control_array, 128, 255)
+        elif ballx > goalx and goalx > 50:
+            #move forward
+            control_array = [enable, 1, 1, 0, 0]
+            send_data(control_array, 0, 128)
+        else:
+            # always send a command to not move to keep-alive connection
+            stop_movement()
+
+    else:
+        stop_movement()
 
     #cv2.imshow('hsv', hsv)
     #cv2.imshow('mask', mask)
