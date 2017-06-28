b03142ad86474a8a7a563ff9d753b029230316db
Dan_Shields <d.shieldsuk@gmail.com>
2017-06-28 14:12:42 +0100

    tcp server test

--- controller.py
+++ controller.py
@@ -1,64 +1,72 @@
 #!/usr/bin/env python
 import numpy as np
 import serial
-import xbox
-joy = xbox.Joystick()
+import socket
 
-ser = serial.Serial('/dev/ttyACM0', 9600, timeout=0.050, bytesize=8)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = socket.gethostname()
+port = 26656
+s.bind((host, port))
 
+s.listen(5)
+while True:
+  c, addr = s.accept()
+  print ('Got connection from',addr)
+  c.send('Thank you for connecting')
+  c.close()
 
+# ser = serial.Serial('/dev/ttyACM0', 9600, timeout=0.050, bytesize=8)
 
-a = np.packbits([0, 1, 1, 1, 1, 1, 1, 1], axis=0)
-b = np.append(a, [255, 255])
+# a = np.packbits([0, 1, 1, 1, 1, 1, 1, 1], axis=0)
+# b = np.append(a, [255, 255])
 
-data = bytearray(iter(b))
-ser.write(data)
-print "Sending handshake"
+# data = bytearray(iter(b))
+# ser.write(data)
+# print "Sending handshake"
 
-enable = 1
+# enable = 1
 
-while not joy.Back():
-    throttle = joy.rightTrigger()
-    brake = joy.leftTrigger()
-    x_axis = joy.leftX()
+# while not joy.Back():
+#     throttle = joy.rightTrigger()
+#     brake = joy.leftTrigger()
+#     x_axis = joy.leftX()
 
-    global_speed = int(abs(throttle - brake) * 255)
-    move = int(global_speed > 20)
-    F_B = int(throttle > brake)
+#     global_speed = int(abs(throttle - brake) * 255)
+#     move = int(global_speed > 20)
+#     F_B = int(throttle > brake)
 
-    turn_speed = 255 - int(abs(x_axis) * 200)
-    L = int(x_axis < 0)
-    R = int(x_axis > 0)
+#     turn_speed = 255 - int(abs(x_axis) * 200)
+#     L = int(x_axis < 0)
+#     R = int(x_axis > 0)
 
-    if F_B == 0 and (L == 1 or R == 1):
-        L = int(not bool(L))
-        R = int(not bool(R))
+#     if F_B == 0 and (L == 1 or R == 1):
+#         L = int(not bool(L))
+#         R = int(not bool(R))
 
-    if not joy.B():
-        global_speed = int(global_speed / 2)
+#     if not joy.B():
+#         global_speed = int(global_speed / 2)
 
-    a = np.packbits([enable, move, F_B, L, R], axis=0)
-    b = np.append(a, [turn_speed, global_speed])
+#     a = np.packbits([enable, move, F_B, L, R], axis=0)
+#     b = np.append(a, [turn_speed, global_speed])
 
-    data = bytearray(iter(b))
+#     data = bytearray(iter(b))
 
-    if move == 1 and F_B == 1:
-        print "Moving forwards"
-    elif move == 1 and F_B == 0:
-        print "Moving backwards"
+#     if move == 1 and F_B == 1:
+#         print "Moving forwards"
+#     elif move == 1 and F_B == 0:
+#         print "Moving backwards"
 
-    #write data if previous data was received
-    if ser.inWaiting() > 0:
-        print "Wrote data"
-        ser.write(data)
-        ser.read()
+#     #write data if previous data was received
+#     if ser.inWaiting() > 0:
+#         print "Wrote data"
+#         ser.write(data)
+#         ser.read()
 
-a = np.packbits([0], axis=0)
-b = np.append(a, [0, 0])
+# a = np.packbits([0], axis=0)
+# b = np.append(a, [0, 0])
 
-data = bytearray(iter(b))
+# data = bytearray(iter(b))
 
-ser.write(data)
+# ser.write(data)
 
-ser.close()
-joy.close()
+# ser.close()
